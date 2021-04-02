79bd98aba08d99fe00366a549870f7df7f5dbe0e
James Hogan <james@albanarts.com>
2021-04-02 22:45:01 +0100

    Recognise patterns and special cases during sort
    When sorting blocks, recognise certain patterns and special cases,
    namely:
     - exact rectangle special cases (just 12=3x4, 16=4x4, 18=4x6, 21=3x7,
       24=3x8 so far).
     - almost rectangles (where an end column or row is incomplete).
     - triangle numbers ("step squads" in Numberblocks). These are counted
       from the smallest step, and the last step may be incomplete.
     - cube special cases (just 27=3x3x3 so far).
    
    This is intended to be as data driven as practical.
    
    Signed-off-by: James Hogan <james@albanarts.com>

--- update.lua
+++ update.lua
@@ -110,24 +110,385 @@ local function numberblocks_add_number(pos, number)
 	end
 end
 
--- Assign an ordering to the nodes
-local function numberblocks_sort_blocks(nodes, count)
-	local i = 0
-	for y, xs in orderedPairs(nodes) do
-		for x, zs in orderedPairs(xs) do
-			for z, info in orderedPairs(zs) do
+local function nodes_size(nodes, range_min, range_max)
+	if range_min == nil then
+		range_min = { x = nil, y = nil, z = nil }
+	end
+	if range_max == nil then
+		range_max = { x = nil, y = nil, z = nil }
+	end
+
+	-- Calculate the range of x, y and z
+	local min = vector.new(0, 0, 0)
+	local max = vector.new(0, 0, 0)
+	local count = 0
+	local first = true
+	for y, xs in pairs(nodes) do
+		if (range_min.y == nil or y >= range_min.y) and
+		   (range_max.y == nil or y <= range_max.y) then
+		   for x, zs in pairs(xs) do
+			   if (range_min.x == nil or x >= range_min.x) and
+				   (range_max.x == nil or x <= range_max.x) then
+				   for z, info in pairs(zs) do
+					   if (range_min.z == nil or z >= range_min.z) and
+						   (range_max.z == nil or z <= range_max.z) then
+						   if info.t == NODE_BLOCK then
+							   count = count + 1
+							   if first then
+								   min = vector.new(x, y, z)
+								   max = vector.new(min)
+								   first = false
+							   else
+								   if z < min.z then
+									   min.z = z
+								   end
+								   if z > max.z then
+									   max.z = z
+								   end
+								   if x < min.x then
+									   min.x = x
+								   end
+								   if x > max.x then
+									   max.x = x
+								   end
+								   if y < min.y then
+									   min.y = y
+								   end
+								   if y > max.y then
+									   max.y = y
+								   end
+							   end
+						   end
+					   end
+				   end
+			   end
+		   end
+	   end
+	end
+	return vector.subtract(max, min), min, max, count
+end
+
+-- Arbitrary sort
+-- e.g. numberblocks_sort({...}, {{'x', 1}, {'y', -1}})
+local function numberblocks_sort(nodes, ordering, numbering)
+	if not ordering then
+		ordering = {}
+	end
+	local reverse_ordering = {}
+	for k = 1, #ordering do
+		reverse_ordering[ordering[k][1]] = k
+	end
+	local default_sort = { 'y', 'x', 'z' }
+	for k = 1, 3 do
+		local v = default_sort[k]
+		if not reverse_ordering[v] then
+			local idx = table.maxn(ordering) + 1
+			ordering[idx] = { v, 1 }
+			reverse_ordering[v] = idx
+		end
+	end
+
+	local snodes = {}
+	for y, xs in pairs(nodes) do
+		for x, zs in pairs(xs) do
+			for z, info in pairs(zs) do
 				if info.t == NODE_BLOCK then
-					nodes[y][x][z].o = i;
-					i = i + 1
+					local pos = { x = x, y = y, z = z }
+					local spos = {
+						pos[ordering[1][1]],
+						pos[ordering[2][1]],
+						pos[ordering[3][1]]
+					}
+
+					if not snodes[spos[1]] then
+						snodes[spos[1]] = {}
+					end
+					if not snodes[spos[1]][spos[2]] then
+						snodes[spos[1]][spos[2]] = {}
+					end
+					snodes[spos[1]][spos[2]][spos[3]] = true
 				end
+			end
+		end
+	end
+	local i = 0
+	for a, bs in orderedPairs(snodes, ordering[1][2]) do
+		for b, cs in orderedPairs(bs, ordering[2][2]) do
+			for c in orderedPairs(cs, ordering[3][2]) do
+				local spos = { a, b, c }
+				local pos = {
+					x = spos[reverse_ordering.x],
+					y = spos[reverse_ordering.y],
+					z = spos[reverse_ordering.z]
+				}
+				if numbering then
+					nodes[pos.y][pos.x][pos.z].o = numbering[i + 1] - 1;
+				else
+					nodes[pos.y][pos.x][pos.z].o = i;
+				end
+				i = i + 1
 			end
 		end
 	end
 end
 
+local dimention_names = { 'x', 'z', 'y' }
+
+local function numberblocks_size_dimentions(size)
+	local dimentions = 0
+	for i, d in ipairs(dimention_names) do
+		if size[d] > 0 then
+			dimentions = dimentions + 1
+		end
+	end
+	return dimentions
+end
+
+-- size is 1 based
+local function numberblocks_is_triangle(nodes, size, min, max)
+	for di, d in ipairs(dimention_names) do
+		if size[d] > 1 then
+			local d2
+			for ddi, dd in ipairs(dimention_names) do
+				if dd ~= d and size[dd] > 1 then
+					d2 = dd
+				end
+			end
+			-- indexed first by 1 for negative, 2 for positive in d direction
+			-- indexed second by 1 for extending negative, 2 for positive
+			local directions = { { true, true }, { true, true } }
+			for i = min[d],max[d] do
+				local range_min = { x = nil, y = nil, z = nil }
+				local range_max = { x = nil, y = nil, z = nil }
+				range_min[d] = i;
+				range_max[d] = i;
+				local size2, min2, max2, count2 = nodes_size(nodes, range_min, range_max)
+
+				-- reject any gaps
+				if count2 ~= max2[d2] - min2[d2] + 1 then
+					return
+				end
+
+				-- check if not growing d2 with increasing d
+				if directions[2][1] or directions[2][2] then
+					if (i ~= max[d] and size2[d2] ~= i-min[d]) or
+					   size2[d2] > i-min[d] then
+					    directions[2] = { false, false }
+					end
+				end
+				-- check if not growing d2 with decreasing d
+				if directions[1][1] or directions[1][2] then
+					if (i ~= min[d] and size2[d2] ~= max[d]-i) or
+					   size2[d2] > max[d]-i then
+					    directions[1] = { false, false }
+					end
+				end
+				-- check if max d2 is aligned
+				if directions[1][2] or directions[2][2] then
+					if max2[d2] ~= max[d2] then
+						directions[1][2] = false
+						directions[2][2] = false
+					end
+				end
+				-- check if min d2 is aligned
+				if directions[1][1] or directions[2][1] then
+					if min2[d2] ~= min[d2] then
+						directions[1][1] = false
+						directions[2][1] = false
+					end
+				end
+			end
+			if directions[1][1] or directions[1][2] or directions[2][1] or directions[2][2] then
+				local dir = -1
+				if directions[2][1] or directions[2][2] then
+					dir = 1
+				end
+				return d, dir
+			end
+		end
+	end
+end
+
+-- [numberblock][width] = { top to bottom, left to right }
+local numberblocks_rect_specials = {
+	[12] = {
+		[3] = { 8,  9, 10,
+		        6, 12,  7,
+				4, 11,  5,
+				1,  2,  3 },
+	},
+	[16] = {
+		[4] = {  7,  8,  9, 10,
+		         3,  4,  5,  6,
+		         1, 15, 16,  2,
+		        11, 12, 13, 14 },
+	},
+	[18] = {
+		[3] = {  9, 10, 18,
+		         7,  8, 17,
+		         5,  6, 16,
+		         3,  4, 15,
+		         1,  2, 14,
+		        11, 12, 13 },
+	},
+	[21] = {
+		[3] = { 18, 19, 20,
+			    15, 16, 17,
+			    12, 13, 14,
+			    11, 21, 10,
+			     7,  8,  9,
+			     4,  5,  6,
+			     1,  2,  3 },
+	},
+	[24] = {
+		[3] = { 18, 19, 20,
+			    15, 16, 17,
+			    13, 24, 14,
+			    11, 23, 12,
+			     9, 22, 10,
+			     7, 21,  8,
+			     4,  5,  6,
+			     1,  2,  3 },
+	},
+}
+
+-- transposed
+local numberblocks_tri_specials = {
+	[28] = {  1,
+              2,  3,
+		      4,  5,  6,
+		      7,  8,  9, 10,
+		     11, 12, 13, 14, 15,
+		     21, 16, 17, 18, 19, 20,
+		     22, 23, 24, 25, 26, 27, 28 },
+}
+
+local numberblocks_cube_specials = {
+	[3] = { -- 27
+		-- 27 is a cube, with corners and edges part of 20
+		-- Number should probably be further forward
+		sorting = { { 'y', 1 }, { 'x', 1 }, { 'z', 1 } },
+		numbering = {  1,  2,  3,   4, 26,  5,   6,  7,  8,		-- bottom
+					   9, 22, 10,  23, 27, 24,  11, 25, 12,		-- mid
+					  13, 14, 15,  16, 21, 17,  18, 19, 20 }	-- top
+	},
+}
+
+-- Assign an ordering to the nodes
+local function numberblocks_sort_blocks(nodes, count)
+	local size, min, max = nodes_size(nodes)
+
+	local dimentions = numberblocks_size_dimentions(size)
+
+	-- offset by 1 for convenience, since zero based sizes are less intuitive
+	size = vector.add(size, vector.new(1, 1, 1))
+
+	if dimentions == 1 then
+		-- 1 dimentional, can't go wrong
+		numberblocks_sort(nodes)
+	elseif dimentions == 2 then
+		-- 2 dimentional, this is where all the interesting stuff is
+		local d1, d2
+		if size.x == 1 then
+			d1 = 'z'
+			d2 = 'y'
+		elseif size.y == 1 then
+			d1 = 'x'
+			d2 = 'z'
+		else
+			d1 = 'x'
+			d2 = 'y'
+		end
+
+		-- rectangles
+		local rectangle_class = 0
+		if count == size[d1] * size[d2] then
+			rectangle_class = 2
+			if numberblocks_rect_specials[count] then
+				if not numberblocks_rect_specials[count][size[d1]] and numberblocks_rect_specials[count][size[d2]] then
+					-- swap to match representation in numberblocks_rect_specials
+					local tmp = d1
+					d1 = d2
+					d2 = tmp
+				end
+				if numberblocks_rect_specials[count][size[d1]] then
+					numberblocks_sort(nodes, { { d2, -1 }, { d1, 1} },
+					                  numberblocks_rect_specials[count][size[d1]])
+					return
+				end
+			end
+		end
+
+		-- default rectangles and almost rectangles
+		local dims = { d1, d2 }
+		for i, dd in ipairs(dims) do
+			local dd2 = dims[3 - i]
+
+			local range_min = vector.new(min)
+			local range_max = vector.new(max)
+			local d2_dir = 0
+			local size2, min2, max2, count2
+			if rectangle_class == 2 then
+				-- already a perfect rectangle
+				size2 = size
+				min2 = min
+				max2 = max
+				count2 = count
+				d2_dir = 1
+			else
+				-- find if a rectangle up to last row
+				range_max[dd2] = range_max[dd2] - 1
+				local size2, min2, max2, count2 = nodes_size(nodes, range_min, range_max)
+				size2 = vector.add(size2, vector.new(1, 1, 1))
+				if size2[dd] == size[dd] and count2 == size2[dd] * size2[dd2] then
+					d2_dir = 1
+				else
+					-- find if a rectangle down to first row
+					range_max[dd2] = range_max[dd2] + 1
+					range_min[dd2] = range_min[dd2] + 1
+					size2, min2, max2, count2 = nodes_size(nodes, range_min, range_max)
+					size2 = vector.add(size2, vector.new(1, 1, 1))
+					if size2[dd] == size[dd] and count2 == size2[dd] * size2[dd2] then
+						d2_dir = -1
+					end
+				end
+			end
+			if d2_dir ~= 0 then
+				local numbering = nil
+				numberblocks_sort(nodes, { { dd2, d2_dir } }, numbering)
+				return
+			end
+		end
+
+		-- triangular numbers
+		local tri_dim, tri_dir = numberblocks_is_triangle(nodes, size, min, max)
+		if tri_dim then
+			numberblocks_sort(nodes, { { tri_dim, tri_dir } }, numberblocks_tri_specials[count])
+			return
+		end
+
+		numberblocks_sort(nodes)
+	elseif dimentions == 3 then
+		-- 3 dimentional
+
+		-- Special cases
+		if numberblocks_cube_specials[size.x] and
+		   size.x == size.y and size.y == size.z and
+		   count == size.x * size.x * size.x then
+			numberblocks_sort(nodes, numberblocks_cube_specials[size.x].sorting,
+							  numberblocks_cube_specials[size.x].numbering);
+			return
+		end
+
+		numberblocks_sort(nodes)
+	end
+end
+
 -- Change the colour of blocks depending on the number of them
 local function numberblocks_restyle_blocks(nodes, count)
-	numberblocks_sort_blocks(nodes, count)
+	if count > 0 then
+		numberblocks_sort_blocks(nodes, count)
+	end
 
 	-- find best place for number nodes
 	local max_y = -31000
